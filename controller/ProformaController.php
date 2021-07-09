1bfa10981caffcfe0d473f596e897699f167f2fa
Alvaro Sosa <alvsosa@alumno.unlam.edu.ar>
2021-07-09 19:39:13 -0300

    validación de proforma en proceso

--- controller/ProformaController.php
+++ controller/ProformaController.php
@@ -18,6 +18,28 @@ class ProformaController
     }
 
     public function validarProforma(){
+        $proforma = array();
+        if(isset($_POST["numero"], $_POST["fecha"])){
+            $numero = $_POST["numero"];
+            $fecha = $_POST["fecha"];
+            $this->model->guardarProforma($numero, $fecha);
+        if(isset($_POST["denominacion"], $_POST["cuit"], $_POST["telefono"], $_POST["email"], $_POST["contacto1"], $_POST["contacto2"])){
+            $denominacion = $_POST["denominacion"];
+            $cuit = $_POST["cuit"];
+            $telefono = $_POST["telefono"];
+            $email = $_POST["email"];
+            $contacto1 = $_POST["contacto1"];
+            $contacto2 = $_POST["contacto2"];
+            $this->model->guardarCliente($denominacion, $cuit, $telefono, $email, $contacto1, $contacto2);
+        }
 
+        if(isset($_POST["origen"], $_POST["destino"], $_POST["fechaCarga"], $_POST["eta"])){
+            $origen = $_POST["origen"];
+            $destino = $_POST["destino"];
+            $fechaCarga = $_POST["fechaCarga"];
+            $eta = $_POST["eta"];
+            $this->model->guardarViaje($origen, $destino, $fechaCarga, $eta);
+        }
+        echo $this->renderer->render("view/login.php", $proforma);
     }
 }
--- view/proforma.php
+++ view/proforma.php
@@ -23,28 +23,19 @@
                         <label>Email:</label>
                         <input class="w3-input" style="width:100%;" type="email" required name="email">
                         <label>Contacto 1:</label>
-                        <input class="w3-input" style="width:100%;" type="number" required name="contacto 1">
+                        <input class="w3-input" style="width:100%;" type="number" required name="contacto1">
                         <label>Contacto 2:</label>
-                        <input class="w3-input" style="width:100%;" type="number" required name="contacto 2">
+                        <input class="w3-input" style="width:100%;" type="number" required name="contacto2">
                     </div>
                     <br>
                     <div class="w3-section">
                         <label>Viaje</label>
                         <label>Origen:</label>
                         <input class="w3-input" style="width:100%;" type="text" required name="origen">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Destino:</label>
                         <input class="w3-input" style="width:100%;" type="text" required name="destino">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Fecha de carga:</label>
-                        <input class="w3-input" style="width:100%;" type="date" required name="fechaDeCarga">
-                    </div>
-                    <br>
-                    <div class="w3-section">
+                        <input class="w3-input" style="width:100%;" type="date" required name="fechaCarga">
                         <label>ETA:</label>
                         <input class="w3-input" style="width:100%;" type="text" required name="eta">
                     </div>
@@ -52,7 +43,7 @@
                     <div class="w3-section">
                         <label>Carga</label>
                         <label>Tipo:</label>
-                        <select name="Tipo">
+                        <select name="tipo">
                             <option value="granel">Granel</option>
                             <option value="liquida" >Líquida</option>
                             <option value="20">20</option>
@@ -60,30 +51,18 @@
                             <option value="jaula">Jaula</option>
                             <option value="CarCarrier">Car Carrier</option>
                         </select>
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Peso neto:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="pesoNeto">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Hazard:</label>
                         <label><input type="radio" name="hazard" value="si"> Si</label><br>
                         <label><input type="radio" name="hazard" value="no"> No</label><br>
-                    </div>
-                    <br>
-                    <div class="w3-section">
+                        <label>Reefer:</label>
+                        <label><input type="radio" name="reefer" value="si"> Si</label><br>
+                        <label><input type="radio" name="reefer" value="no"> No</label><br>
                         <label>IMO class:</label>
                         <input class="w3-input" style="width:100%;" type="text" required name="imoClass">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>IMO Sclass:</label>
                         <input class="w3-input" style="width:100%;" type="text" required name="imoSclass">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Temperatura:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="temperatura">
                     </div>
@@ -95,75 +74,51 @@
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoKm">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realKm">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Combustible:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoCombustible">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realCombustible">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>ETD:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoEtd">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realEtd">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>ETA:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoEta">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realEta">
-                    </div>
-                    <br>
-                    <div class="w3-section">
                         <label>Viaticos:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoViaticos">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realViaticos">
-                    </div>
-                    <div class="w3-section">
                         <label>Peajes y pasajes:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoPasajes">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realPasajes">
-                    </div>
-                    <div class="w3-section">
                         <label>Extras:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoExtras">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realExtras">
-                    </div>
-                    <div class="w3-section">
                         <label>Hazard:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoHhazard">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realHazard">
-                    </div>
-                    <div class="w3-section">
                         <label>Reefer:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoRefeer">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realReefer">
-                    </div>
-                    <div class="w3-section">
                         <label>Free:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoFree">
                         <label>Real:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="realFree">
-                    </div>
-                    <div class="w3-section">
                         <label>Total:</label>
                         <label>Estimado:</label>
                         <input class="w3-input" style="width:100%;" type="number" required name="estimadoTotal">
