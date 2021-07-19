0869141933f4638205aa886deaf16867b855ae14
Alvaro Sosa <alvsosa@alumno.unlam.edu.ar>
2021-07-19 15:07:31 -0300

    funcionalidad qr

---
+++ view/cogigo-qr.php
@@ -0,0 +1,13 @@
+{{>headerAdmin}}
+{{>navAdmin}}
+<div class="w3-panel">
+    <h5>Codigo QR</h5>
+    {{#qr}}
+    <div class="w3-table w3-striped w3-white">
+
+        {{qr}}
+
+    </div>
+    {{/qr}}
+</div>
+{{>footerAdmin}}
